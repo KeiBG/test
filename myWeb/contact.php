38902d0d60b47b66a312b71c69b85c4ed94c3e43
KeiBG <78953540+KeiBG@users.noreply.github.com>
2021-10-08 16:49:50 +0900

    Add files via upload

---
+++ myWeb/contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myWeb index</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="header-logo">CCC</div>
+        <div class="header-menues">
+            <ul>
+                <li>Home</li>
+                <li class="selected">Profile</li>
+                <li>Game</li>
+                <li>Bass</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="contents">
+        
+    </div>
+
+    <div class="footer">
+        <div class="footer-logo">CCC</div>
+        <div class="footer-menues">
+            <ul>
+                <li>Home</li>
+                <li>Profile</li>
+                <li>Game</li>
+                <li>Bass</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
---
+++ myWeb/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myWeb index</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="header-logo">CCC</div>
+        <div class="header-menues">
+            <ul>
+                <li class="selected">Home</li>
+                <li><a href="./profile.php">Profile</a></li>
+                <li><a href="./game.php">Game</a></li>
+                <li><a href="./bass.php">Bass</a></li>
+                <li><a href="./contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="contents">
+        
+    </div>
+
+    <div class="footer">
+        <div class="footer-logo">CCC</div>
+        <div class="footer-menues">
+            <ul>
+                <li>Home</li>
+                <li>Profile</li>
+                <li>Game</li>
+                <li>Bass</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
